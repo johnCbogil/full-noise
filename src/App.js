3e088c28e928850007bb7915f91a4aece03e0721
John Bogil <johncbogil@gmail.com>
2022-12-06 21:52:27 -0500

    can't forge tthis

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { Outlet, Link } from "react-router-dom";
+import axios from "axios"
 
 export default function App() {
+   const [posts, setPosts] = useState([]);
+
    useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   });
+      axios
+      .get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      .then(response => {
+         console.log(response)
+         setPosts(response.data)
+      })
+      .catch(error => {
+         console.log(error)
+      })
+   }, [])
 
    return (
-    <></>
+      <div>
+      <ul>
+         {
+            // TODO: path here should not be hardcoded, need to pass data via loader and/or action
+            posts.map(post => <Link to={`contacts/1`}><li key={post.id}>{post.title}</li></Link>)
+         }
+      </ul>
+      </div>
     );
 };
