aee857fad3ca33abe2666b4a926a05b776f32e0d
John Bogil <johncbogil@gmail.com>
2022-12-06 20:10:41 -0500

    Revert "Start building the router"
    This reverts commit 05078b5e7d7ea98309bdbdd31a019499549c4608.

--- src/PostDetail.js
+++
@@ -1,3 +0,0 @@
-export const PostDetail = () => {
-    return <>PostDetail</>
-}
--- src/Posts.js
+++
@@ -1,9 +0,0 @@
-export const Posts = () => {
-    return( 
-    <>
-        <h2>Post 1</h2>
-        <h2>Post 2</h2>
-        <h2>Post 3</h2>
-    </>
-    )
-}
